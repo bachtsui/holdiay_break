3c5a10629bc08a19e5227ccb86b927e0904f485f
bachtsui <dbtsui2@gmail.com>
2015-12-24 15:03:50 -0800

    repeatPhrase done

--- ruby-method-challenges.rb
+++ ruby-method-challenges.rb
@@ -1,8 +1,19 @@
+# 1.Concat
 
-def combineWords(word1 , word2)
-	puts word1 + word2 
-	return word1 + word2
-end
+# def combineWords(word1 , word2)
+# 	puts word1 + word2 
+# 	return word1 + word2
+# end
 
-combineWords('dog','house')
+# combineWords('dog','house')
+
+#2. Repeat Phrase
+
+# def repeatPhrase (phrase, n)
+# 	puts phrase * n
+# end
+
+# repeatPhrase("Hello", 5)
+
+# Might need to add line breaks, not sure yet
 

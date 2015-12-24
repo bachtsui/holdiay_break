68124d175f35afac32c77d72828b4761589d4c6d
bachtsui <dbtsui2@gmail.com>
2015-12-24 15:38:27 -0800

    Learned about variable declaration and while loops

--- ruby-method-challenges.rb
+++ ruby-method-challenges.rb
@@ -17,3 +17,21 @@
 
 # Might need to add line breaks, not sure yet
 
+# 3. Build Power Function
+
+def toTheNthPower(number, power)
+	result = 1
+
+	if power == 0
+		puts 1
+	else
+		i = 0
+		while (i < power)
+			result *= number
+			i+=1
+			puts result
+		end
+	end
+end
+
+toTheNthPower(4, 5)
